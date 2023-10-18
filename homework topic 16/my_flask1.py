603865867e0424490d8acf91968758f93d511570
KirraDark <blackbloodyring@gmail.com>
2023-10-18 14:07:24 +0300

    flask

---
+++ homework topic 16/my_flask1.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+
+@app.route('/life')
+def life():
+    return render_template('life.html')
+
+
+
+app.run(port=8080)
+
